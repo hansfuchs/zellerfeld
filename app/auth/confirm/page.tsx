4d0685c4e75471c5584b2cac58b82f114d40e53e
Hans Fuchs <f.hanschristian@gmail.com>
2026-01-16 02:50:51 +0100

    fix wrongful use of router

--- app/auth/confirm/page.tsx
+++ app/auth/confirm/page.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 import Link from "next/link";
 import Button from "@/lib/components/ui/Button";
@@ -9,19 +9,28 @@ export default async function ConfirmPage({
     query: Promise<{ confirmation_url: string | undefined }>;
 }) {
     const { confirmation_url } = await query;
-    const router = useRouter();
 
-    if (!confirmation_url) {
-        router.push("/");
-    }
+    const [success, setSuccess] = useState(false);
 
     return (
         <>
-            <div>Awesome! Your account was sucessfully confirmed.</div>
+            {confirmation_url ? (
+                <Link href={confirmation_url} className="mt-8">
+                    <Button>Confirm account</Button>
+                </Link>
+            ) : (
+                <div>Invalid confirmation url.</div>
+            )}
 
-            <Link href="/" className="mt-8">
-                <Button>Go to feed</Button>
-            </Link>
+            {success && (
+                <>
+                    <div>Awesome! Your account was sucessfully confirmed.</div>
+
+                    <Link href="/" className="mt-8">
+                        <Button>Go to feed</Button>
+                    </Link>
+                </>
+            )}
         </>
     );
 }
