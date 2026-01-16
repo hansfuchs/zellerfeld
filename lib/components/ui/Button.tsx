af7181785fb7c6d576d93f68eb91fbb1d1a0db6b
Hans Fuchs <f.hanschristian@gmail.com>
2026-01-16 03:21:01 +0100

    set ui components as client-side

--- lib/components/ui/Button.tsx
+++ lib/components/ui/Button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 interface Props {
     type?: "button" | "submit";
     disabled?: boolean;
--- lib/components/ui/Input.tsx
+++ lib/components/ui/Input.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 interface Props {
     type: string;
     value: string;
