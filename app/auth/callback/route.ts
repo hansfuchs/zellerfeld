886d87ce082b51346dc20176751b2dc2e0e2c3f5
Hans Fuchs <f.hanschristian@gmail.com>
2026-01-18 21:00:51 +0100

    try out OTP

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,17 +1,19 @@
 import { NextResponse } from "next/server";
 import { createDBClient } from "@/lib/db/db.server";
+import type { EmailOtpType } from "@supabase/supabase-js";
 
 export async function GET(request: Request) {
     const { searchParams, origin } = new URL(request.url);
-    const code = searchParams.get("code");
+    const token_hash = searchParams.get("token_hash");
+    const type = searchParams.get("type") as EmailOtpType | null;
 
-    if (!code) {
+    if (!token_hash || !type) {
         return NextResponse.redirect(`${origin}/?error=auth_callback_error`);
     }
 
     const supabase = await createDBClient();
 
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    const { error } = await supabase.auth.verifyOtp({ token_hash, type });
     if (error) {
         return NextResponse.redirect(`${origin}/?error=auth_callback_error`);
     }
